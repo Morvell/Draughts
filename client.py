7081576ffd6f62eb2b8d6f2d9a7770e3cb49651a
morvell <zaxarov2702@yandex.ru>
2017-04-20 14:07:30 +0500

    упрощено

--- client.py
+++ client.py
@@ -1,21 +1,71 @@
-import socket
-import string
-import base64
+class Client:
+    """
+    Класс клиент. Осуществляет отправку данных
+    """
+
+    def __init__(self):
+        """
+        Создает сокет и отправляет сокету-получателю свои данные(порт и ip)
+        """
+        try:
+            self.s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+            self.s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+            self.s.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+            self.s.bind((netveriable.YOUR_IP, netveriable.YOUR_PORT))
+        except socket.error:
+            print('Failed to create socket')
+            sys.exit()
+
+        self.s.sendto(
+            json.dumps(('ready', netveriable.NICKNAME)).encode(),
+            (netveriable.SEND_IP, netveriable.SEND_PORT))
+        print("You may send are message")
+
+    def send_array(self):
+        """
+        обрабатывает ввод сообщения,
+        файла и отправляет сообшение всем подключенным пользователям
+        """
+        global polemass
+        mass = self.polemassparser(polemass)
+        self.s.sendto(
+            json.dumps(mass).encode(),
+            (netveriable.SEND_IP, netveriable.SEND_PORT))
+
+    def next_hod(self):
+        """
+        отравляет опоненту ообщение о том что он может начинать ходить
+        """
+        global polemass
+        mass = self.polemassparser(polemass)
+        self.s.sendto(
+            json.dumps(('next', mass)).encode(),
+            (netveriable.SEND_IP, netveriable.SEND_PORT))
+
+    def polemassparser(self, mass):
+        """
+        парсит массив классов в массив строк для передачи по сети
+        :param mass: массив который надо парсить(с данными о доске)
+        :return итоговый распарсенный массив
+        """
+        self.polemass = []
+        for i in range(10):
+            self.polemass.append([])
+            for j in range(10):
+                self.polemass[i].append([])
+
+        for i in range(10):
+            for j in range(10):
+                if type(mass[i][j]) == Hahki.Kletka:
+                    self.polemass[i][j] = ("Kletka|" + str(mass[i][j].x) + '|' + str(mass[i][j].y))
+                else:
+                    self.polemass[i][j] = (
+                    "Hahka|" + str(mass[i][j].x) + '|' + str(mass[i][j].y) + '|' + str(mass[i][j].vid) + '|' +
+                    str(mass[i][j].side) + '|' + str(mass[i][j].damka))
+        return self.polemass
 
 
-s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-done = True
-while done:
-    try:
-        s.connect(('localhost', 60001))
-    except:
-        continue
-    done = False
-    print('connect')
 
-while True:
-    message = input()
-    s.send(message.encode())
-print('all send')
-s.close()
+
+
 
--- server.py
+++ server.py
@@ -1,26 +1,90 @@
 
+class Server(threading.Thread):
+    """
+    Сервер. Получает данные с других сокетов и обрабатывает их
+    """
 
-import socket
-import string
-import base64
+    def __init__(self):
+        """
+            инициализирут сокет для получения данных
+            """
+        super().__init__()
+        try:
+            self.s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+            self.s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+            self.s.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+            print('Socket created')
+        except socket.error as msg:
+            print('Failed to create socket. Error Code : ')
+            sys.exit()
+        try:
+            self.s.bind((netveriable.YOUR_IP, netveriable.YOUR_PORT))
+        except socket.error as msg:
+            print('Bind failed. Error Code : ')
+            sys.exit()
+        print('Socket bind complete')
 
+    def run(self):
+        """
+        получает данные и обрабатывает их. если поевляется новый пользователь,
+        отправляет существующим новый список участников
+        А так же обрабатывает получение файла
+        """
+        print('1')
+        while True:
+            try:
+                self.streamdata = self.s.recvfrom(4096)
+                self.data = json.loads(self.streamdata[0].decode())
+                self.addr = self.streamdata[1]
 
+                if isinstance(self.data, list) and len(self.data) == 2 and self.data[0] == 'ready':
 
-s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-s.bind(('localhost', 60001))
-s.listen(2)
+                    netveriable.ENEMY_NICKNAME = self.data[1]
+                    self.s.sendto(json.dumps(('ready', netveriable.NICKNAME)).encode(),
+                                  (netveriable.SEND_IP, netveriable.SEND_PORT))
+                    netveriable.ENEMY_READY = True
+                elif isinstance(self.data, list) and len(self.data) == 2 and self.data[0] == 'next':
+                    print('next')
 
+                    global polemass
+                    global gochess
+                    global playerchess
+                    self.polemassdeparser(self.data[1], polemass)
+                    gochess = playerchess
+                elif isinstance(self.data, list) and len(self.data) == 1:
 
+                    self.polemassdeparser(self.data, polemass)
 
-connect,adress = s.accept()
-while 1:
+                elif isinstance(self.data, str) and self.data == 'exit':
 
-    data=connect.recv(1024)
-    print(data.decode())
+                    netveriable.ENEMY_READY = False
+                    netveriable.NETWORK_READY = False
 
+            except Exception as e:
+                print(e)
 
-
-print("all get")
-s.close()
+    def polemassdeparser(self, mass, polemass):
+        """
+        :param mass: массив полученный после polymassparser
+        :param polemass: массив в который будем распарсевать
+        """
+        for i in range(10):
+            for j in range(10):
+                smass = mass[i][j].split('|')
+                if smass[0] == 'Kletka':
+                    polemass[i][j] = Hahki.Kletka(int(smass[1]), int(smass[2]))
+                else:
+                    if smass[3] == 'black' and smass[5] == 'False':
+                        polemass[i][j] = Hahki.Hahka(int(smass[1]), int(smass[2]), 'black', i_hb, smass[4])
+                        polemass[i][j].damka = False
+                    elif smass[3] == 'white' and smass[5] == 'False':
+                        polemass[i][j] = Hahki.Hahka(int(smass[1]), int(smass[2]), 'white', i_hw, smass[4])
+                        polemass[i][j].damka = False
+                    elif smass[3] == 'black' and smass[5] == 'True':
+                        polemass[i][j] = Hahki.Hahka(int(smass[1]), int(smass[2]), 'black', i_db, smass[4])
+                        polemass[i][j].damka = True
+                    elif smass[3] == 'white' and smass[5] == 'True':
+                        polemass[i][j] = Hahki.Hahka(int(smass[1]), int(smass[2]), 'white', i_dw, smass[4])
+                        polemass[i][j].damka = True
 
 
