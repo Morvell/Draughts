8cfaf530eba0719fb022139afc086de0c5699d02
morvell <zaxarov2702@yandex.ru>
2017-04-25 14:12:58 +0500

    исправлено отображение кто ходит

--- Hahkiapi.py
+++ Hahkiapi.py
@@ -13,14 +13,108 @@ last_kill = "kletka"
 labelWhite = font.render(str(numberOfWhite), 1, (255, 255, 255))
 labelBlack = font.render(str(numberOfBlack), 1, (0, 0, 0))
 
+labelWhiteChess = font.render("Белые", 1, (255, 255, 255))
+labalBlackChess = font.render("Черные", 1, (0, 0, 0))
+
 side = 'down'
 playerchess = 'black'
 gochess = 'white'
+
+polemass = []
+
+mouse_button_down_fl = False
+continuehod = False
+continuehahka = None
+
+ipos = 0
+jpos = 0
 
 lengthOrWidth = 72
 
 i_db = pygame.image.load('pic/DBlack.gif')
 i_dw = pygame.image.load('pic/DWhite.gif')
+
+def whoGo(surface):
+    if gochess == "white":
+        surface.blit(labelWhiteChess, (25, 300))
+    else:
+        surface.blit(labalBlackChess, (15, 300))
+
+def without_net(mp):
+    global continuehod
+    global continuehahka
+    global mouse_button_down_fl
+    global gochess
+    global ipos
+    global jpos
+    for i in range(10):
+        for j in range(10):
+            if checkchess(mp, polemass, i, j):
+                if continuehod == True and polemass[i][j] == continuehahka:
+                    print('in False')
+                    mouse_button_down_fl = True
+                    continuehod = False
+                    ipos = i
+                    jpos = j
+                elif continuehod == True and polemass[i][j] != continuehahka:
+                    break
+                # блок выбора шашки
+                elif (mouse_button_down_fl == False or polemass[i][j].vid == gochess) and polemass[i][
+                    j].vid != 'kletka' and polemass[i][j].vid == gochess:
+                    if len(check_chess_with_enemy(polemass, gochess)) != 0:
+                        if check_correct_chess(polemass, check_chess_with_enemy(polemass, gochess), polemass[i][j]):
+
+                            print('in False')
+                            mouse_button_down_fl = True
+                            ipos = i
+                            jpos = j
+                            break
+                        else:
+                            break
+                    else:
+                        print('in False')
+                        mouse_button_down_fl = True
+
+                        ipos = i
+                        jpos = j
+                        break
+                # блок хода
+                elif mouse_button_down_fl == True and gochess != polemass[i][j].vid:
+                    mass = check_enemy(polemass, polemass[ipos][jpos], ipos, jpos)
+                    if len(mass) != 0:
+                        if hod_with_enemy(polemass, mass, ipos, jpos, i, j):
+                            changeNumber()
+
+                            mouse_button_down_fl = False
+                            set_damka(polemass, i, j)
+
+                            if len(check_enemy(polemass, polemass[i][j], i, j)) == 0:
+                                set_damka(polemass, i, j)
+                                if gochess == 'white':
+                                    gochess = 'black'
+                                else:
+                                    gochess = 'white'
+                            else:
+                                set_damka(polemass, i, j)
+                                continuehod = True
+                                continuehahka = polemass[i][j]
+                            break
+
+
+                    elif (check_hod_without_enemy(polemass[ipos][jpos], polemass[i][j]) or (
+                                polemass[ipos][jpos].damka and check_correct_damka_hod(polemass[ipos][jpos],
+                                                                                       polemass[i][j]))) and \
+                                    polemass[i][
+                                        j].vid == 'kletka':
+                        print('in True')
+                        mouse_button_down_fl = False
+                        hod(polemass, ipos, jpos, i, j)
+                        set_damka(polemass, i, j)
+                        if gochess == 'white':
+                            gochess = 'black'
+                        else:
+                            gochess = 'white'
+                        break
 
 def changeNumberRender(surface):
     surface.blit(labelWhite, (70, 20))
--- main.py
+++ main.py
@@ -6,81 +6,7 @@ from menu import *
 pygame.init()
 
 
-def without_net():
-    global continuehod
-    global continuehahka
-    global mouse_button_down_fl
-    global gochess
-    global ipos
-    global jpos
-    for i in range(10):
-        for j in range(10):
-            if checkchess(mp, polemass, i, j):
-                if continuehod == True and polemass[i][j] == continuehahka:
-                    print('in False')
-                    mouse_button_down_fl = True
-                    continuehod = False
-                    ipos = i
-                    jpos = j
-                elif continuehod == True and polemass[i][j] != continuehahka:
-                    break
-                # блок выбора шашки
-                elif (mouse_button_down_fl == False or polemass[i][j].vid == gochess) and polemass[i][
-                    j].vid != 'kletka' and polemass[i][j].vid == gochess:
-                    if len(check_chess_with_enemy(polemass, gochess)) != 0:
-                        if check_correct_chess(polemass, check_chess_with_enemy(polemass, gochess), polemass[i][j]):
 
-                            print('in False')
-                            mouse_button_down_fl = True
-                            ipos = i
-                            jpos = j
-                            break
-                        else:
-                            break
-                    else:
-                        print('in False')
-                        mouse_button_down_fl = True
-
-                        ipos = i
-                        jpos = j
-                        break
-                # блок хода
-                elif mouse_button_down_fl == True and gochess != polemass[i][j].vid:
-                    mass = check_enemy(polemass, polemass[ipos][jpos], ipos, jpos)
-                    if len(mass) != 0:
-                        if hod_with_enemy(polemass, mass, ipos, jpos, i, j):
-                            changeNumber()
-
-                            mouse_button_down_fl = False
-                            set_damka(polemass, i, j)
-
-                            if len(check_enemy(polemass, polemass[i][j], i, j)) == 0:
-                                set_damka(polemass, i, j)
-                                if gochess == 'white':
-                                    gochess = 'black'
-                                else:
-                                    gochess = 'white'
-                            else:
-                                set_damka(polemass, i, j)
-                                continuehod = True
-                                continuehahka = polemass[i][j]
-                            break
-
-
-                    elif (check_hod_without_enemy(polemass[ipos][jpos], polemass[i][j]) or (
-                                polemass[ipos][jpos].damka and check_correct_damka_hod(polemass[ipos][jpos],
-                                                                                       polemass[i][j]))) and \
-                                    polemass[i][
-                                        j].vid == 'kletka':
-                        print('in True')
-                        mouse_button_down_fl = False
-                        hod(polemass, ipos, jpos, i, j)
-                        set_damka(polemass, i, j)
-                        if gochess == 'white':
-                            gochess = 'black'
-                        else:
-                            gochess = 'white'
-                        break
 
 
 board = pygame.image.load('pic/Board.gif')
@@ -90,7 +16,7 @@ i_menu = pygame.image.load('pic/menu.png')
 
 i_rightscreen = pygame.image.load('pic/rightscreen.png')
 
-polemass = []
+
 
 # Блок выбора цвета и стороны шашек
 # side - сторона на которой будут находится белые шашки down или up
@@ -99,12 +25,7 @@ polemass = []
 startpos(polemass, side, (i_hb, i_hw, i_db, i_dw))
 done = True
 
-mouse_button_down_fl = False
-continuehod = False
-continuehahka = None
 
-ipos = 0
-jpos = 0
 
 punkts = [(400, 200, u'Start', (123, 15, 34), (235, 75, 156), 0),
           (400, 400, u'Exit', (123, 15, 34), (235, 75, 156), 2)]
@@ -131,13 +52,12 @@ while done:
             done = False
 
         if e.type == pygame.MOUSEBUTTONDOWN and e.button == 1:
-            without_net()
+            without_net(mp)
 
     # render text
 
     labalGoChess = font.render("Ходят", 1, (78, 226, 14))
-    labelWhiteChess = font.render("Белые", 1, (255, 255, 255))
-    labalBlackChess = font.render("Черные", 1, (0, 0, 0))
+    
 
     window.blit(mainscreen, (0, 0))
     window.blit(rightscreen, (720, 0))
@@ -145,10 +65,8 @@ while done:
     mainscreen.blit(board, (0, 0))
     changeNumberRender(rightscreen)
     rightscreen.blit(labalGoChess, (25, 220))
-    if gochess == "white":
-        rightscreen.blit(labelWhiteChess, (25, 300))
-    else:
-        rightscreen.blit(labalBlackChess, (15, 300))
+    whoGo(rightscreen)
+
 
     for i in range(10):
         for j in range(10):
