d0364aab21081747763b8890ec1828c822a99eb6
morvell <zaxarov2702@yandex.ru>
2017-12-06 21:54:41 +0500

    fix test

--- test_HahkiAPI.py
+++ test_HahkiAPI.py
@@ -1,14 +1,216 @@
+import unittest
 from unittest import TestCase
 
-from DraughtsAPI import DraughtsAPI
+import DraughtsAPI as dAPI
 
 
 class TestHahkiAPI(TestCase):
-    game = DraughtsAPI()
+    game = dAPI.DraughtsAPI()
+
+    def test_draughts_with_normal_step(self):
+        self.game.gameField = [
+            list(' . . w . .'),
+            list('w . b b . '),
+            list(' . . . . .'),
+            list('. . . . . '),
+            list(' . . . . .'),
+            list('. . . . . '),
+            list(' . . . . v'),
+            list('. . . v . '),
+            list(' . . v . v'),
+            list('q . . v v '),
+        ]
+
+        self.assertTrue(self.game.draughts_with_normal_step(1, 0))
+        self.assertFalse(self.game.draughts_with_normal_step(0, 5))
+
+    def test_check_end_game(self):
+        self.game.numberOfWhite = 0
+        self.game.playerDraughts = "w"
+        self.assertEqual(self.game.check_end_game(), (True, "lose"))
+
+        self.game.numberOfWhite = 0
+        self.game.playerDraughts = "b"
+        self.assertEqual(self.game.check_end_game(), (True, "win"))
+
+        self.game.numberOfBlack = 0
+        self.game.numberOfWhite = 1
+        self.game.playerDraughts = "b"
+        self.assertEqual(self.game.check_end_game(), (True, "lose"))
+
+        self.game.numberOfBlack = 0
+        self.game.numberOfWhite = 1
+        self.game.playerDraughts = "w"
+        self.assertEqual(self.game.check_end_game(), (True, "win"))
+
+        self.game.numberOfBlack = 1
+        self.game.numberOfWhite = 1
+        self.game.playerDraughts = "b"
+        self.assertEqual(self.game.check_end_game(), (False, False))
+
+    def test_check_draughts(self):
+        mp = (0, 1)
+        self.assertEqual(self.game.check_draughts(mp), (0, 0))
+
+    def test_set_start_field(self):
+        gameField = [
+            list(' b b b b b'),
+            list('b b b b b '),
+            list(' b b b b b'),
+            list('b b b b b '),
+            list(' . . . . .'),
+            list('. . . . . '),
+            list(' w w w w w'),
+            list('w w w w w '),
+            list(' w w w w w'),
+            list('w w w w w '),
+        ]
+        self.game.set_start_playing_field()
+        self.assertEqual(self.game.gameField, gameField)
+
+        gameField = [
+            list(' w w w w w'),
+            list('w w w w w '),
+            list(' w w w w w'),
+            list('w w w w w '),
+            list(' . . . . .'),
+            list('. . . . . '),
+            list(' b b b b b'),
+            list('b b b b b '),
+            list(' b b b b b'),
+            list('b b b b b '),
+        ]
+
+        self.game.set_start_playing_field("up")
+        self.assertEqual(self.game.gameField, gameField)
+
+    def test_isKing(self):
+        self.game.iFirstAP = 0
+        self.game.jFirstAP = 1
+
+        self.game.gameField = [
+            list(' v b q . .'),
+            list('v . . q . '),
+            list(' . . . . q'),
+            list('. . . . . '),
+            list(' . . . . .'),
+            list('. . . . . '),
+            list(' . . . . v'),
+            list('. . . v . '),
+            list(' . . v . v'),
+            list('q . . v v '),
+        ]
+
+        self.assertTrue(self.game.is_king())
+
+        self.game.iFirstAP = 0
+        self.game.jFirstAP = 3
+
+        self.assertFalse(self.game.is_king())
+
+        self.game.iFirstAP = 0
+        self.game.jFirstAP = 5
+
+        self.assertTrue(self.game.is_king())
+
+    def test_check_correct_step_for_king(self):
+        self.game.playDraughts = 'b'
+        self.game.playerDraughts = 'b'
+        self.game.iFirstAP = 0
+        self.game.jFirstAP = 1
+        self.game.gameField = [
+            list(' v . . . .'),
+            list('. . . . . '),
+            list(' . . . . .'),
+            list('. . . . . '),
+            list(' . . . . .'),
+            list('. . . . . '),
+            list(' . . . . .'),
+            list('. . . . . '),
+            list(' . . . . .'),
+            list('. . . . . '),
+        ]
+
+        self.assertTrue(self.game.check_correct_step_for_king(1, 2))
+        self.assertFalse(self.game.check_correct_step_for_king(0, 2))
+        self.assertFalse(self.game.check_correct_step_for_king(0, 2))
+
+        self.game.playDraughts = "w"
+        self.game.iFirstAP = 0
+        self.game.jFirstAP = 3
+
+        self.game.gameField = [
+            list(' . q . . .'),
+            list('. . . . . '),
+            list(' . . w w .'),
+            list('. . . . . '),
+            list(' . . . . .'),
+            list('. . . . . '),
+            list(' . . . . .'),
+            list('. . . . . '),
+            list(' . . . . .'),
+            list('. . . . . '),
+        ]
+        self.assertTrue(self.game.check_correct_step_for_king(1, 2))
+        self.assertFalse(self.game.check_correct_step_for_king(4, 8))
+
+    def test_load(self):
+        self.game.numberOfWhite = 20
+        self.game.numberOfBlack = 19
+        self.game.playDraughts = "b"
+        self.game.continueStep = False
+        self.game.gameField = [
+            list(' . . . . .'),
+            list('v . . q . '),
+            list(' . . . . q'),
+            list('. . . . . '),
+            list(' . . . . .'),
+            list('. . . . . '),
+            list(' . . . . v'),
+            list('. . . v . '),
+            list(' . . v . v'),
+            list('q . . v v '),
+        ]
+
+        save = self.game.save_game()
+        self.game.numberOfWhite = 15
+        self.game.numberOfBlack = 12
+        self.game.playDraughts = "w"
+        self.game.continueStep = True
+
+        self.game.load_game(save)
+
+        self.assertTrue(self.game.playDraughts, "b")
+        self.assertTrue(self.game.numberOfBlack, "19")
+        self.assertTrue(self.game.numberOfWhite, "20")
+        self.assertFalse(self.game.continueStep)
+
+    def test_toBool(self):
+        self.assertTrue(self.game.to_bool("True"))
+        self.assertFalse(self.game.to_bool("False"))
+
+    def test_save(self):
+        self.game.numberOfWhite = 20
+        self.game.numberOfBlack = 20
+
+        self.game.gameField = [
+            list(' . . . . .'),
+            list('v . . q . '),
+            list(' . . . . q'),
+            list('. . . . . '),
+            list(' . . . . .'),
+            list('. . . . . '),
+            list(' . . . . v'),
+            list('. . . v . '),
+            list(' . . v . v'),
+            list('q . . v v '),
+        ]
+        save = self.game.save_game()
+        self.assertEqual(self.game.save_game(), save)
 
     def test_hodWithEnemyForDamka(self):
-        self.game.iFirstActivePosition = 0
-        self.game.jFirstActivePosition = 9
+        self.game.iFirstAP = 0
+        self.game.jFirstAP = 9
         self.game.gameField = [
             list(' . . . . q'),
             list('. . . b . '),
@@ -37,8 +239,8 @@ class TestHahkiAPI(TestCase):
         self.game.step_with_enemy_for_king(2, 5)
         self.assertEqual(self.game.gameField, accesmass)
         # For black damka
-        self.game.iFirstActivePosition = 2
-        self.game.jFirstActivePosition = 5
+        self.game.iFirstAP = 2
+        self.game.jFirstAP = 5
 
         self.game.gameField = [
             list(' . . . . .'),
@@ -69,8 +271,8 @@ class TestHahkiAPI(TestCase):
         self.assertEqual(self.game.gameField, accesmassForBlack)
 
     def test_accesHodForDamka(self):
-        self.game.iFirstActivePosition = 2
-        self.game.jFirstActivePosition = 5
+        self.game.iFirstAP = 2
+        self.game.jFirstAP = 5
 
         self.game.playDraughts = 'b'
         self.game.playerDraughts = 'w'
@@ -90,8 +292,8 @@ class TestHahkiAPI(TestCase):
 
         self.assertTrue(self.game.correct_hod_for_king(0, 7))
 
-        self.game.iFirstActivePosition = 0
-        self.game.jFirstActivePosition = 7
+        self.game.iFirstAP = 0
+        self.game.jFirstAP = 7
 
         self.game.gameField = [
             list(' . . . v .'),
@@ -108,8 +310,8 @@ class TestHahkiAPI(TestCase):
 
         self.assertTrue(self.game.correct_hod_for_king(2, 5))
 
-        self.game.iFirstActivePosition = 3
-        self.game.jFirstActivePosition = 4
+        self.game.iFirstAP = 3
+        self.game.jFirstAP = 4
 
         self.game.gameField = [
             list(' . . . . .'),
@@ -179,8 +381,8 @@ class TestHahkiAPI(TestCase):
             list('w w w w w '),
         ]
 
-        self.assertTrue(self.game.friend_or_not(0, 1))
-        self.assertTrue(self.game.friend_or_not(0, 3))
+        self.assertTrue(self.game.is_friend(0, 1))
+        self.assertTrue(self.game.is_friend(0, 3))
 
         self.game.playDraughts = "w"
 
@@ -197,8 +399,8 @@ class TestHahkiAPI(TestCase):
             list('w w w w w '),
         ]
 
-        self.assertTrue(self.game.friend_or_not(1, 0))
-        self.assertTrue(self.game.friend_or_not(1, 2))
+        self.assertTrue(self.game.is_friend(1, 0))
+        self.assertTrue(self.game.is_friend(1, 2))
 
     def test_setDamka(self):
         self.game.playDraughts = 'w'
@@ -291,8 +493,8 @@ class TestHahkiAPI(TestCase):
         self.game.playDraughts = "b"
         self.game.playerDraughts = "b"
 
-        self.game.iFirstActivePosition = 0
-        self.game.jFirstActivePosition = 7
+        self.game.iFirstAP = 0
+        self.game.jFirstAP = 7
 
         self.game.gameField = [
             list(' . . . b .'),
@@ -312,14 +514,13 @@ class TestHahkiAPI(TestCase):
         self.assertTrue(self.game.gameField[1][6] == '.')
         self.assertTrue(self.game.gameField[0][7] == '.')
 
-        self.game.iFirstActivePosition = 8
-        self.game.jFirstActivePosition = 1
+        self.game.iFirstAP = 8
+        self.game.jFirstAP = 1
 
         self.game.step_with_enemy(6, 3)
         self.assertTrue(self.game.gameField[6][3] == 'w')
         self.assertTrue(self.game.gameField[7][2] == '.')
         self.assertTrue(self.game.gameField[8][1] == '.')
-
 
     def test_checkEnemy(self):
         self.game.gameField = [
@@ -336,8 +537,7 @@ class TestHahkiAPI(TestCase):
         ]
 
         self.assertEqual(self.game.check_enemy(0, 7), [(1, 6)])
-        self.assertEqual(self.game.check_enemy(5,4), [(6,3)])
-
+        self.assertEqual(self.game.check_enemy(5, 4), [(6, 3)])
 
     def test_checkChessWithEnemy(self):
         self.game.playDraughts = "b"
@@ -354,7 +554,8 @@ class TestHahkiAPI(TestCase):
             list('. . . . . '),
         ]
 
-        self.assertEqual(self.game.check_chess_with_enemy(), [(0, 3), (0, 7), (5, 4)])
+        self.assertEqual(self.game.check_chess_with_enemy(),
+                         [(0, 3), (0, 7), (5, 4)])
 
     def test_normalHodRule(self):
         self.game.playerDraughts = "w"
@@ -387,7 +588,6 @@ class TestHahkiAPI(TestCase):
         self.game.playDraughts = 'b'
         self.game.change_number_of_live_draughts()
         self.assertEqual(self.game.numberOfWhite, 19)
-
 
     def test_how_kill(self):
         self.game.playDraughts = "w"
@@ -435,8 +635,12 @@ class TestHahkiAPI(TestCase):
             list(' b . b b b'),
             list('b b b b b '),
         ]
-        self.game.iFirstActivePosition=6
-        self.game.jFirstActivePosition=3
-        self.assertEqual(self.game.correct_step_with_enemy(6,3),[])
-        self.assertEqual(self.game.correct_step_with_enemy(4,5),[(4, 1), (4, 5), (8, 1), (8, 5)])
+        self.game.iFirstAP = 6
+        self.game.jFirstAP = 3
+        self.assertEqual(self.game.correct_step_with_enemy(6, 3), [])
+        self.assertEqual(self.game.correct_step_with_enemy(4, 5),
+                         [(4, 1), (4, 5), (8, 1), (8, 5)])
 
+
+if __name__ == '__main__':
+    unittest.main()
